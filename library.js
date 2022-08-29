790bfe5a20110848f47248fa276f249d41b177f3
cbroddle <christenbroddle@gmail.com>
2022-08-29 16:48:43 -0700

    Menu and form click functions added, styling in progress

--- library.js
+++ library.js
@@ -1,3 +1,25 @@
+// menu icon toggle
+const menuToggle = document.querySelector(".menu-icon");
+// menu
+const menuSidebar = document.querySelector(".menu-container");
+
+// add new book button
+const newBookBtn = document.querySelector(".add-button");
+
+const newBookForm = document.querySelector(".add-form");
+
+
+// menu event listener
+menuToggle.addEventListener("click", function () {
+    menuSidebar.classList.toggle("show-menu-container");
+});
+
+// display "new book" form
+newBookBtn.addEventListener("click", function () {
+    newBookForm.classList.toggle("show-add-form");
+});
+
+/*
 let myLibrary = [];
 
 // Constructor function
@@ -16,3 +38,4 @@ const myBook = new Book("Heart of Darkness,", "by Joseph Conrad,", "116 pages,",
 
 // Print book info
 myBook.info()
+*/
