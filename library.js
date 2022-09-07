52d2c48bc85e393974551e1edd1d87fbf63f931f
cbroddle <christenbroddle@gmail.com>
2022-09-06 20:33:31 -0700

    Added form page 2 with some links functional

--- library.js
+++ library.js
@@ -3,6 +3,10 @@ const menuToggle = document.querySelector(".menu-icon");
 // menu
 const menuSidebar = document.querySelector(".menu-container");
 
+// read checkbox?
+// Trashcan Icon - removes card
+const trashIcon = document.querySelector(".trash-icon");
+
 // add new book button
 const newBookBtn = document.querySelector(".add-button");
 
@@ -12,6 +16,14 @@ const modalOverlay = document.querySelector(".modal-overlay");
 
 // cancel "add new book" link
 const cancelLink = document.querySelector(".cancel-link");
+
+// next page of "add book" link
+const nextLink = document.querySelector(".next-link");
+
+const prevLink = document.querySelector(".prev-link");
+
+// show second page of form (reading question)
+const newBookFormRead = document.querySelector(".add-form-two");
 
 // menu event listener
 menuToggle.addEventListener("click", function () {
@@ -30,6 +42,17 @@ newBookBtn.addEventListener("click", function () {
 cancelLink.addEventListener("click", function () {
     newBookForm.classList.remove("show-add-form");
     modalOverlay.classList.remove("show-modal-overlay");
+})
+
+//show page 2 of form
+nextLink.addEventListener("click", function () {
+    newBookFormRead.classList.toggle("show-add-form-two");
+    //modalOverlay.classList.toggle("show-modal-overlay");
+})
+
+// Go back to page 1 of form
+prevLink.addEventListener("click", function () {
+    newBookFormRead.classList.remove("show-add-form-two");
 })
 
 /*
